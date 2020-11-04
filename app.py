a3fe16d824f911ade8d9e5a228b7e1efa674e8b3
baturayo <baturay@dataroots.io>
2020-11-04 10:20:33 +0100

    add dploy.yaml and /healtzh/ endpoint

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+@app.route('/healthz/', methods=['GET'])
+def health_check():
+    return "healthy"
+
+
 @app.route('/')
 def hello():
     return "Hello World!"
